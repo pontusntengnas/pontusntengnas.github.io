9dbcace02173fe6b922441482c1233d26b3e08c9
pontusntengnas <pontusnt@gmail.com>
2019-06-17 21:00:27 +0200

    fix lint warnings

--- src/main.ts
+++ src/main.ts
@@ -21,9 +21,9 @@ const routes = [
 
 const router = new VueRouter({
   routes,
-  scrollBehavior (to, from, savedPosition) {
+  scrollBehavior(to, from, savedPosition) {
     return { x: 0, y: 0 };
-  }
+  },
 });
 
 new Vue({
